e5e18c12ac73b44c7ce31cf66870cb0d8cb1bb76
morris <morris881961@gmail.com>
2021-06-23 14:55:35 +0800

    add voting and recent

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -1,6 +1,7 @@
 // import models
 import Group from '../models/group.js';
 import User from '../models/user.js';
+import Discussion from '../models/discussion.js';
 
 // function for every cases
 async function index({ UID }) {
@@ -11,6 +12,7 @@ async function index({ UID }) {
   var group = [];
   var error_msg = "Something wrong...";
   try {
+    const now = new Date().getTime();
     const user = await User.findById(UID);
     if (!user) {
       status = false;
@@ -21,8 +23,40 @@ async function index({ UID }) {
     var groups = user.groups;
     for (let i = 0; i < groups.length; i++) {
       const aGroup = await Group.findById(groups[i]);
-      group.push({GID: aGroup._id, GName: aGroup.GName});
+      group.push({ GID: aGroup._id, GName: aGroup.GName });
+      const discussions = aGroup.discussions;
+      for (let j = 0; j < discussions.length; j++) {
+        const aDiscussion = await Discussion.findById(discussions[j])
+        // before deadline
+        if ((now - aDiscussion.deadline.getTime()) < 0) {
+          voting.push({
+            GID: aGroup._id,
+            DID: aDiscussion._id,
+            GName: aGroup.GName,
+            subject: aDiscussion.subject,
+            time: aDiscussion.time_result,
+            place: aDiscussion.place_result,
+            deadline: aDiscussion.deadline,
+          })
+        }
+        // after deadline
+        else {
+          // before discussion time result
+          if (now - aDiscussion.time_result.getTime() < 0) {
+            recent.push({
+              GID: aGroup._id,
+              DID: aDiscussion._id,
+              GName: aGroup.GName,
+              subject: aDiscussion.subject,
+              time: aDiscussion.time_result,
+              place: aDiscussion.place_result,
+            })
+          }
+        }
+      }
     }
+    voting.sort((a, b) => (a.deadline.getTime() < b.deadline.getTime()) ? 1 : ((b.deadline.getTime() < a.deadline.getTime()) ? -1 : 0))
+    recent.sort((a, b) => (a.time_result.getTime() < b.time_result.getTime()) ? 1 : ((b.time_result.getTime() < a.time_result.getTime()) ? -1 : 0))
     status = true
     error_msg = "Successed!";
   } catch (e) {
