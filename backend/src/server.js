bc3ac9aa65c36f49c1f21eb3b1f1da5b5f3aebe4
morris <morris881961@gmail.com>
2021-06-24 19:09:55 +0800

    test deploy

--- backend/src/server.js
+++ backend/src/server.js
@@ -212,7 +212,7 @@ wss.on('connection', async function connection(ws) {
   });
 });
 
-const PORT = process.env.port || 4000
+const PORT = process.env.PORT || 4000
 server.listen(PORT, () => {
   console.log(`Listening on http://localhost:${PORT}`)
 })
