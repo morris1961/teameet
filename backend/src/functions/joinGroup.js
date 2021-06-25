00d4fe49f6339b5691cf8455981b965032781d10
morris <morris881961@gmail.com>
2021-06-25 11:53:09 +0800

    return only GID

--- backend/src/functions/joinGroup.js
+++ backend/src/functions/joinGroup.js
@@ -6,7 +6,6 @@ import User from '../models/user.js';
 async function joinGroup({ UID, code }) {
   var status = false;
   var GID = "";
-  var GName = [];
   var error_msg = "Something wrong...";
   try {
     const group = await Group.findOne({ code });
@@ -22,19 +21,9 @@ async function joinGroup({ UID, code }) {
       return { status, error_msg };
     }
     var groups = user.groups;
-    for (let i = 0; i < groups.length; i++) {
-      if (groups[i].toString() === group._id.toString()) {
-        status = false;
-        error_msg = "The user has been in the group!"
-        return { status, error_msg };
-      }
-      const aGroup = await Group.findById(groups[i]);
-      GName.push(aGroup.GName);
-    }
     groups.push(group._id);
     await User.updateOne({ _id: UID }, { $set: { groups } });
-    const aGroup = await Group.findById(group._id);
-    GName.push(aGroup.GName);
+    GID = group._id;
     status = true;
     error_msg = "Successed!";
   } catch (e) {
@@ -42,7 +31,7 @@ async function joinGroup({ UID, code }) {
     status = false;
     error_msg = "Something wrong...";;
   }
-  return { status, GID, GName, error_msg };
+  return { status, GID, error_msg };
 }
 
 export default joinGroup;
