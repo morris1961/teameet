f1122d6c1a2e04776b92a9645f4c6c421f8b3bca
hsinweiw122489 <winniew0824@gmail.com>
2021-06-25 15:28:05 +0800

    Fixed voting and recent of homepage

--- frontend/src/Components/RecentGroups.js
+++ frontend/src/Components/RecentGroups.js
@@ -1,14 +1,15 @@
-import React from 'react'
+import React, { useEffect } from 'react';
 import '../style/RecentGroups.css';
+import {useState} from 'react';
+import moment from 'moment';
+import 'moment-timezone';
 const RecentGroups = (props) =>{
-
-
     return( 
         <React.Fragment>
           <div className="recentgroups_button">
           {props.GName}
             <div >
-                <div>時間: {props.time}</div>
+                <div>時間: {moment(props.time_result).tz('Asia/Taipei').format('YYYY-MM-DD HH:mm')}</div>
                 <div>地點: {props.place}</div>
                 <div>主題: {props.subject}</div>
             </div>
