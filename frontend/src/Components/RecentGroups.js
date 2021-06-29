bacc80b4a9671513b77ba4c6e09583d889ae51c4
yuchen3890 <b07705047@ntu.edu.tw>
2021-06-29 17:46:38 +0800

    adjust groups render

--- frontend/src/Components/RecentGroups.js
+++ frontend/src/Components/RecentGroups.js
@@ -2,15 +2,26 @@ import React from 'react';
 import '../style/RecentGroups.css';
 import moment from 'moment';
 import 'moment-timezone';
+import { Tag } from 'antd';
+
 const RecentGroups = (props) =>{
     return( 
         <React.Fragment>
           <div className="recentgroups_button">
-          {props.GName}
-            <div >
-                <div>時間: {moment(props.time_result).tz('Asia/Taipei').format('YYYY-MM-DD HH:mm')}</div>
-                <div>地點: {props.place}</div>
-                <div>主題: {props.subject}</div>
+          <h3 style={{textShadow: "1px 1px 0px rgba(0,0,0,0.2)", color: "#003060"}}>群組：{props.GName}</h3>
+            <div>
+                <div>
+                  <Tag color="blue">討論主題</Tag>
+                  {props.subject}
+                </div>
+                <div>
+                  <Tag color="blue">投票時間</Tag>
+                  {moment(props.time).tz('Asia/Taipei').format('YYYY-MM-DD HH:mm')} 
+                </div>
+                <div>
+                  <Tag color="blue">地點</Tag>
+                  {props.place}
+                </div>
             </div>
           </div>
         </React.Fragment>  
--- frontend/src/Components/VotingGroups.js
+++ frontend/src/Components/VotingGroups.js
@@ -2,16 +2,22 @@ import React from 'react';
 import '../style/RecentGroups.css';
 import moment from 'moment';
 import 'moment-timezone';
+import { Tag } from 'antd';
 
 const VotingGroups = (props) =>{
     return( 
         <React.Fragment>
           <div className="recentgroups_button">
-          {props.GName}
-            <div >
-                <div>投票截止時間: {moment(props.deadline).tz('Asia/Taipei').format('YYYY-MM-DD HH:mm')} </div>
-                <div>地點: {props.place}</div>
-                <div>主題: {props.subject}</div>
+          <h3 style={{textShadow: "1px 1px 0px rgba(0,0,0,0.2)", color: "#003060"}}>群組：{props.GName}</h3>
+            <div>
+                <div>
+                  <Tag color="blue">討論主題</Tag>
+                  {props.subject}
+                </div>
+                <div>
+                  <Tag color="blue">投票截止時間</Tag>
+                  {moment(props.deadline).tz('Asia/Taipei').format('YYYY-MM-DD HH:mm')} 
+                </div>
             </div>
           </div>
         </React.Fragment>  
